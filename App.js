d791c2d4af2099ba4b84b03cb96b1b68aeca0ed3
ZedPetero <peterozed@gmail.com>
2026-03-24 09:29:34 +0800

    Finished chapter 2

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 const app = Vue.createApp({
   data () {
     return {
-      time: 143
+      showBooks: true,
+      books: [
+        {title: 'What moves the dead', author: 'T. Kingfisher', img: 'assets/1.jpg', isFav: true},
+        {title: 'What feasts at night', author: 'T. Kingfisher', img: 'assets/2.jpg', isFav: false},
+        {title: 'What stalks the deep', author: 'T. Kingfisher', img: 'assets/3.jpg', isFav: true},
+      ]
     }
-  }
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav
+    }
+  } 
 })
 
 app.mount('#app')
